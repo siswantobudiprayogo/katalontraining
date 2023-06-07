9d81b61153ebfe973470570f51819f8f2e8065f6
cobasajagit <dianayuu1234@gmail.com>
2023-06-08 01:14:51 +0700

    dian - added Mobile Test Suite and Run for Report

---
+++ Test Suites/SCH_HEROKU_MOBILE_TestSuite.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SCH_HEROKU_MOBILE_TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>156b667c-b222-4352-a3d0-39cb1eaec4ab</testSuiteGuid>
+   <testCaseLink>
+      <guid>819ea474-4458-4de5-a273-414f59404064</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/sch_heroku_mobile_1_login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bdbbbd7e-0968-468c-84bb-9302b5437636</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/sch_heroku_mobile_2_jualbarang</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
